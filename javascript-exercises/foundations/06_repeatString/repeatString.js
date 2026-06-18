a93a544e4d8b3b1c8e703fd3849f05aa2d4edf4a
Renato-Barbosa-Medeiros <renatobamedeiros@gmail.com>
2026-06-18 15:12:32 -0300

    Fix the nested repository issue

---
+++ javascript-exercises/foundations/06_repeatString/repeatString.js
@@ -0,0 +1,13 @@
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    } 
+    let string = "";
+    for (i=0; i < num; i++) {
+        string += word;
+    }
+    return string;
+};
+
+// Do not edit below this line
+module.exports = repeatString;
