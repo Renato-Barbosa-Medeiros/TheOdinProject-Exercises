88c3bc34778b081262e5daa78dac97775fe00026
Renato-Barbosa-Medeiros <renatobamedeiros@gmail.com>
2026-06-25 15:09:15 -0300

    JS Foundation exercises 13 and 14 completed

--- javascript-exercises/foundations/13_palindromes/palindromes.js
+++ javascript-exercises/foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const lettersAndNumbers = 'abcdefghijklmnopqrstuvwxyz1235467890';
+    const onlyCharsString = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => lettersAndNumbers.includes(character))
+    .join('');
 
+    const reversedString = onlyCharsString.split('').reverse().join('');
+
+    return onlyCharsString === reversedString; 
 };
 
 // Do not edit below this line
--- javascript-exercises/foundations/14_fibonacci/fibonacci.js
+++ javascript-exercises/foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let num1 = 0, num2 = 0, numNovo = 1
+    if (typeof num !== 'number') {
+        num = parseInt(num)
+    }
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
 
+    for (let i = 0; i < num - 1; i++) {
+        num2 = num1;
+		num1 = numNovo;
+		numNovo = num1 + num2;
+    }
+
+    return numNovo;
 };
 
 // Do not edit below this line
