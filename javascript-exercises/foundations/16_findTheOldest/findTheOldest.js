e2ebb4f81a54e9883e47a21dbc848a43dbe96a84
Renato-Barbosa-Medeiros <renatobamedeiros@gmail.com>
2026-06-26 14:56:00 -0300

    JS Foundations all exercises completed

--- javascript-exercises/foundations/16_findTheOldest/findTheOldest.js
+++ javascript-exercises/foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const getAge = function (birthYear, deathYear) {
+    if (!deathYear) {
+      deathYear = new Date().getFullYear();
+    }
 
-};
+    return deathYear - birthYear;
+
+  };
+  
+  const findTheOldest = function (people) {
+    return people.reduce((oldest, current) => {
+      const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+      const currentAge = getAge(
+        current.yearOfBirth,
+        current.yearOfDeath
+      );
+      return oldestAge < currentAge ? current : oldest;
+
+    });
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
